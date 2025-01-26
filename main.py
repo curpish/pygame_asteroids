d9ab44c6240cb9933b74cfdd05dba85f7f0b67cc
curpish <ravenoftheredsky@gmail.com>
2025-01-26 13:02:08 -0500

    Shapes, can draw player

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -12,20 +13,27 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # Instantiate Player object
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         # Fill the screen with black
         screen.fill((0, 0, 0))
+
+        # Draw the player sprite
+        player.draw(screen)
+
+        # Move to the next frame
         pygame.display.flip()
 
         # Update dt with delta time after limiting to 60 FPS
         dt = clock.tick(60) / 1000
 
-        # Output the current FPS
-        fps = clock.get_fps()
-        print(f"FPS: {fps}")
+        # Output the current FPS in the console
+        # fps = clock.get_fps()
+        # print(f"FPS: {fps}")
 
 if __name__ == "__main__":
     main()
